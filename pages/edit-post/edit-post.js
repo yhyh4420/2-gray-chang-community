03a5d13d34e870aca01f2f35a78df19963879101
Yehun <yhyh4420@naver.com>
2025-02-19 02:21:05 +0900

    feat: 모달페이지 로직 기능 추가

---
+++ pages/edit-post/edit-post.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const title = document.getElementById("title-field")
+    const content = document.getElementById("content-field")
+    
+    const finishButton = document.querySelector(".finish-posting")
+
+    function validEmpty(title, content){
+        return title.value != "" && content.value != ""
+    }
+
+    function updateButtonState(){
+        const isNotEmpty = validEmpty(title, content)
+        if (isNotEmpty) {
+            finishButton.style.backgroundColor = "#7F6AEE"
+            finishButton.style.cursor = "pointer"
+            finishButton.disabled = false;
+            finishButton.addEventListener("click", function(){
+                window.location.href = '/pages/post-detail/post-detail.html'
+            })
+        } else {
+            finishButton.style.backgroundColor = "#ACA0EB"
+            finishButton.style.cursor = "default"
+            finishButton.disabled = true;
+        }
+    }
+
+    title.addEventListener("input", function(){
+        updateButtonState()
+    })
+
+    content.addEventListener("input", function(){
+        updateButtonState()
+    })
+})
